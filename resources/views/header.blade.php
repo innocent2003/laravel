f9b04393cc4a9cf8bc3e8a274ad48f96e0a83124
Le Tuan <leminhtuanthpttranhungdaotx@gmail.com>
2024-04-20 15:56:46 +0700

    check

---
+++ resources/views/header.blade.php
@@ -0,0 +1,27 @@
+<header>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Logo</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link " aria-current="page" href="./Post.html">Quản lý bài đăng</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Quản lý khai báo</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./Delivery.html">Quản lý giao nhận</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Quản trị hệ thống</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
